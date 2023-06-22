b66f8f1e7e1fd2dd2602ae393c3310b7066b0e95
nikitarsh <nikitaroshmakov@gmail.com>
2023-06-22 22:58:04 +0400

    Исправление линтера #2

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 // Функция для проверки длины строки.
-function checkStringLength (string, maxlength ) {
+function checkStringLength (string, maxlength) {
   return string.length <= maxlength;
 }
-console.log(checkStringLength('Проверка длинны строки', 30));
+// console.log(checkStringLength('Проверка длинны строки', 30));
 // Функция для проверки, является ли строка палиндромом.
 function isPalindrome(str) {
   str = str.replaceAll(' ', '').toUpperCase();
   return str === [...str].reverse().join('');
 }
-console.log(isPalindrome('Лёша на полке клопа нашёл'));
+// console.log(isPalindrome('Лёша на полке клопа нашёл'));
