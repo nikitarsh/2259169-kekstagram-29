824aa5c0124bd05e27ff0641673146789c7cd18c
nikitarsh <nikitaroshmakov@gmail.com>
2023-06-22 22:52:11 +0400

    Исправление замечаний

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,12 @@
 // Функция для проверки длины строки.
-
-function stringLenght (line, Maxlength ) {
-  return line.length <= Maxlength;
+function checkStringLength (string, maxlength ) {
+  return string.length <= maxlength;
 }
-
-console.log(stringLenght('Проверка длинны строки', 30));
-
+console.log(checkStringLength('Проверка длинны строки', 30));
 // Функция для проверки, является ли строка палиндромом.
 
-function palindrome(str) {
+function isPalindrome(str) {
   str = str.replaceAll(' ', '').toUpperCase();
   return str === [...str].reverse().join('');
 }
-
-console.log( palindrome('Лёша на полке клопа нашёл') );
+console.log(isPalindrome('Лёша на полке клопа нашёл'));
