1375c0cf230baee8aa91ac2f3a5a4c909a7fffe6
nikitarsh <nikitaroshmakov@gmail.com>
2023-07-05 15:10:42 +0400

    Исправления линтера

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-import {getRandomArrayElement, getRandomInteger} from "./util.js";
+import {getRandomArrayElement, getRandomInteger} from './util.js';
 
 const NAMES = [
   'Регина',
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {photos} from "./data.js";
+import {photos} from './data.js';
 
 // eslint-disable-next-line no-console
 console.log(photos);
