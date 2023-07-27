1d6727d83769cd525e3537fd8305a3106abb9944
nikitarsh <nikitaroshmakov@gmail.com>
2023-07-27 16:18:48 +0400

    lint fix

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,12 @@
+import { openBigImage } from './big-images.js';
+import { createImages } from './create-miniature-images.js';
 const container = document.querySelector('.pictures');
 
 const renderGallery = (pictures) => {
   container.addEventListener('click', (evt) => {
     const element = evt.target.closest('[data-element-id]');
     if (!element) {
-      return
+      return;
     }
 
     evt.preventDefault();
