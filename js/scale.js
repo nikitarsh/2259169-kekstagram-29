74f6781b7eb23a70738046b8e57fd2146f5716f3
nikitarsh <nikitaroshmakov@gmail.com>
2023-08-08 20:50:18 +0400

    lint fix 2

--- js/scale.js
+++ js/scale.js
@@ -11,29 +11,29 @@ const scaleInput = document.querySelector('.scale__control--value');
 const photoPreview = document.querySelector('.img-upload__preview img');
 
 const scalePhoto = (value) => {
-  scaleInput.value = `${value}%`
+  scaleInput.value = `${value}%`;
   photoPreview.style.scale = value / 100;
-}
+};
 
 const onDecreaseButtonClick = () => {
   const currentValue = parseInt (scaleInput.value, 10);
   const newValue = Math.max(currentValue - imageScale.STEP, imageScale.MIN);
   scalePhoto(newValue);
-}
+};
 
 const onIncreaseButtonClick = () => {
   const currentValue = parseInt (scaleInput.value, 10);
   const newValue = Math.min(currentValue + imageScale.STEP, imageScale.MAX);
   scalePhoto(newValue);
-}
+};
 
 const defaultScale = () => {
   scalePhoto(imageScale.DEFAULT)
-}
+};
 
 const setScale = () => {
   smallerButton.addEventListener('click', onDecreaseButtonClick);
-  biggerButton.addEventListener('click', onIncreaseButtonClick)
-}
+  biggerButton.addEventListener('click', onIncreaseButtonClick);
+};
 
 export {setScale, defaultScale};
