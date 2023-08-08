77eeed29987c93bef6424ebfbeed947c08f75f56
nikitarsh <nikitaroshmakov@gmail.com>
2023-08-08 14:58:52 +0400

    lint fix 1

--- js/filters.js
+++ js/filters.js
@@ -1,51 +1,51 @@
 const FILTERS = {
-    none: {
-        name: 'none',
-        min: 1,
-        max: 100,
-        step: 1,
-        start: 100,
-    },
-    chrome: {
-        name: 'chrome',
-        style: 'grayscale',
-        min: 0,
-        max: 1,
-        step: 0.1,
-        unit: '',
-    },
-    sepia: {
-        name: 'sepia',
-        style: 'sepia',
-        min: 0,
-        max: 1,
-        step: 0.1,
-        unit: '',
-    },
-    marvin: {
-        name: 'marvin',
-        style: 'invert',
-        min: 0,
-        max: 100,
-        step: 1,
-        unit: '%',
-    },
-    phobos: {
-        name: 'phobos',
-        style: 'blur',
-        min: 0,
-        max: 3,
-        step: 0.1,
-        unit: 'px',
-    },
-    heat: {
-        name: 'heat',
-        style: 'brightness',
-        min: 1,
-        max: 3,
-        step: 0.1,
-        unit: '',
-    }
+  none: {
+    name: 'none',
+    min: 1,
+    max: 100,
+    step: 1,
+    start: 100,
+  },
+  chrome: {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  sepia: {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  marvin: {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  phobos: {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  heat: {
+    name: 'heat',
+    style: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  }
 };
 
 const DEFAULT_FILTER = FILTERS.none;
@@ -98,7 +98,7 @@ const onFiltersUpdate = () => {
   uploadPrewiew.style.filter = 'none';
   uploadPrewiew.className = '';
   filterLevelValue.value = '';
-  if(currentFilter === DEFAULT_FILTER) {
+  if (currentFilter === DEFAULT_FILTER) {
     return;
   }
   const sliderValue = filterSlider.noUiSlider.get();
